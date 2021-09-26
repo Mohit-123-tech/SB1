50977b4f4aa7f093351a62737e70ecdab9c870d2
Maikal <82205460+Mohit-123-tech@users.noreply.github.com>
2021-09-26 10:02:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+// import bottom from './App/bottom';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1><i>Hello i am maikal from <b>Silicon Brains</b></i></h1>
+        <img src={'./icon.png'}></img>
+        <button><i><b><a href='https://discord.gg/4SJ8NSRr'>Discord</a></b></i></button>
+        <br></br>
+        <button><i><b>Instagrame</b></i></button>
+        <br></br>
+        <button><i><b><a href='https://github.com/Silicon-Brains'>Github</a></b></i></button>
+        <b><i><h1>Hello Coder Join Discord For Learn Code With Me</h1></i></b>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/App/bottom.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './bottom.css';
+
+function bottom() {
+    return(
+        <div className='bottom'>
+            <h1>Hello World</h1>
+        </div>        
+    );
+}
+
+export default bottom;
